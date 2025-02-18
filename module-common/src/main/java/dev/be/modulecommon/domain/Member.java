231e06262edf4d9c6c3587a90d1b7665f69e6ce9
mini0-0 <nalala8200@naver.com>
2025-02-18 17:32:09 +0900

    Spring Mutil Module

---
+++ module-common/src/main/java/dev/be/modulecommon/domain/Member.java
@@ -0,0 +1,20 @@
+package dev.be.modulecommon.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+}
